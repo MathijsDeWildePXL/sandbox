433451cbbdf1ebfe69d0ea5c9cfe03d77e523f30
Mathijs De Wilde <>
2025-03-05 10:22:20 +0100

    Pipeline fix

---
+++ frontend/src/App.vue.js
@@ -0,0 +1,146 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var HeaderComponent_vue_1 = require("./components/HeaderComponent.vue");
+var MainComponent_vue_1 = require("./components/MainComponent.vue");
+var AsideComponent_vue_1 = require("./components/AsideComponent.vue");
+var ItemComponent_vue_1 = require("./components/ItemComponent.vue");
+var ArticleComponent_vue_1 = require("./components/ArticleComponent.vue");
+debugger; /* PartiallyEnd: #3632/scriptSetup.vue */
+var __VLS_ctx = {};
+var __VLS_components;
+var __VLS_directives;
+/** @type {__VLS_StyleScopedClasses['logo']} */ ;
+// CSS variable injection 
+// CSS variable injection end 
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(__assign({ class: "flex grow flex-col justify-center gap-2 text-black" }));
+/** @type {[typeof Header, typeof Header, ]} */ ;
+// @ts-ignore
+var __VLS_0 = __VLS_asFunctionalComponent(HeaderComponent_vue_1.default, new HeaderComponent_vue_1.default(__assign({ class: "flex justify-center bg-green-500 text-black" })));
+var __VLS_1 = __VLS_0.apply(void 0, __spreadArray([__assign({ class: "flex justify-center bg-green-500 text-black" })], __VLS_functionalComponentArgsRest(__VLS_0), false));
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(__assign({ class: "flex gap-4" }));
+/** @type {[typeof Main, typeof Main, ]} */ ;
+// @ts-ignore
+var __VLS_3 = __VLS_asFunctionalComponent(MainComponent_vue_1.default, new MainComponent_vue_1.default(__assign({ class: "flex grow-2 flex-col items-center bg-gray-500 p-2" })));
+var __VLS_4 = __VLS_3.apply(void 0, __spreadArray([__assign({ class: "flex grow-2 flex-col items-center bg-gray-500 p-2" })], __VLS_functionalComponentArgsRest(__VLS_3), false));
+__VLS_5.slots.default;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(__assign({ class: "flex w-full flex-col gap-2" }));
+/** @type {[typeof Article, typeof Article, ]} */ ;
+// @ts-ignore
+var __VLS_6 = __VLS_asFunctionalComponent(ArticleComponent_vue_1.default, new ArticleComponent_vue_1.default(__assign({ class: "grow bg-purple-500" })));
+var __VLS_7 = __VLS_6.apply(void 0, __spreadArray([__assign({ class: "grow bg-purple-500" })], __VLS_functionalComponentArgsRest(__VLS_6), false));
+__VLS_8.slots.default;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({});
+var __VLS_8;
+/** @type {[typeof Article, typeof Article, ]} */ ;
+// @ts-ignore
+var __VLS_9 = __VLS_asFunctionalComponent(ArticleComponent_vue_1.default, new ArticleComponent_vue_1.default(__assign({ class: "grow bg-purple-500" })));
+var __VLS_10 = __VLS_9.apply(void 0, __spreadArray([__assign({ class: "grow bg-purple-500" })], __VLS_functionalComponentArgsRest(__VLS_9), false));
+__VLS_11.slots.default;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({});
+var __VLS_11;
+var __VLS_5;
+/** @type {[typeof Aside, typeof Aside, ]} */ ;
+// @ts-ignore
+var __VLS_12 = __VLS_asFunctionalComponent(AsideComponent_vue_1.default, new AsideComponent_vue_1.default(__assign({ class: "flex grow-1 flex-col items-center bg-green-300 p-2" })));
+var __VLS_13 = __VLS_12.apply(void 0, __spreadArray([__assign({ class: "flex grow-1 flex-col items-center bg-green-300 p-2" })], __VLS_functionalComponentArgsRest(__VLS_12), false));
+__VLS_14.slots.default;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)(__assign({ class: "flex w-full flex-col gap-2" }));
+/** @type {[typeof Item, typeof Item, ]} */ ;
+// @ts-ignore
+var __VLS_15 = __VLS_asFunctionalComponent(ItemComponent_vue_1.default, new ItemComponent_vue_1.default(__assign({ class: "bg-blue-600" })));
+var __VLS_16 = __VLS_15.apply(void 0, __spreadArray([__assign({ class: "bg-blue-600" })], __VLS_functionalComponentArgsRest(__VLS_15), false));
+__VLS_17.slots.default;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({});
+var __VLS_17;
+/** @type {[typeof Item, typeof Item, ]} */ ;
+// @ts-ignore
+var __VLS_18 = __VLS_asFunctionalComponent(ItemComponent_vue_1.default, new ItemComponent_vue_1.default(__assign({ class: "bg-blue-600" })));
+var __VLS_19 = __VLS_18.apply(void 0, __spreadArray([__assign({ class: "bg-blue-600" })], __VLS_functionalComponentArgsRest(__VLS_18), false));
+__VLS_20.slots.default;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({});
+var __VLS_20;
+/** @type {[typeof Item, typeof Item, ]} */ ;
+// @ts-ignore
+var __VLS_21 = __VLS_asFunctionalComponent(ItemComponent_vue_1.default, new ItemComponent_vue_1.default(__assign({ class: "bg-blue-600" })));
+var __VLS_22 = __VLS_21.apply(void 0, __spreadArray([__assign({ class: "bg-blue-600" })], __VLS_functionalComponentArgsRest(__VLS_21), false));
+__VLS_23.slots.default;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.p, __VLS_intrinsicElements.p)({});
+var __VLS_23;
+var __VLS_14;
+/** @type {__VLS_StyleScopedClasses['flex']} */ ;
+/** @type {__VLS_StyleScopedClasses['grow']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex-col']} */ ;
+/** @type {__VLS_StyleScopedClasses['justify-center']} */ ;
+/** @type {__VLS_StyleScopedClasses['gap-2']} */ ;
+/** @type {__VLS_StyleScopedClasses['text-black']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex']} */ ;
+/** @type {__VLS_StyleScopedClasses['justify-center']} */ ;
+/** @type {__VLS_StyleScopedClasses['bg-green-500']} */ ;
+/** @type {__VLS_StyleScopedClasses['text-black']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex']} */ ;
+/** @type {__VLS_StyleScopedClasses['gap-4']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex']} */ ;
+/** @type {__VLS_StyleScopedClasses['grow-2']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex-col']} */ ;
+/** @type {__VLS_StyleScopedClasses['items-center']} */ ;
+/** @type {__VLS_StyleScopedClasses['bg-gray-500']} */ ;
+/** @type {__VLS_StyleScopedClasses['p-2']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex']} */ ;
+/** @type {__VLS_StyleScopedClasses['w-full']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex-col']} */ ;
+/** @type {__VLS_StyleScopedClasses['gap-2']} */ ;
+/** @type {__VLS_StyleScopedClasses['grow']} */ ;
+/** @type {__VLS_StyleScopedClasses['bg-purple-500']} */ ;
+/** @type {__VLS_StyleScopedClasses['grow']} */ ;
+/** @type {__VLS_StyleScopedClasses['bg-purple-500']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex']} */ ;
+/** @type {__VLS_StyleScopedClasses['grow-1']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex-col']} */ ;
+/** @type {__VLS_StyleScopedClasses['items-center']} */ ;
+/** @type {__VLS_StyleScopedClasses['bg-green-300']} */ ;
+/** @type {__VLS_StyleScopedClasses['p-2']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex']} */ ;
+/** @type {__VLS_StyleScopedClasses['w-full']} */ ;
+/** @type {__VLS_StyleScopedClasses['flex-col']} */ ;
+/** @type {__VLS_StyleScopedClasses['gap-2']} */ ;
+/** @type {__VLS_StyleScopedClasses['bg-blue-600']} */ ;
+/** @type {__VLS_StyleScopedClasses['bg-blue-600']} */ ;
+/** @type {__VLS_StyleScopedClasses['bg-blue-600']} */ ;
+var __VLS_dollars;
+var __VLS_self = (await Promise.resolve().then(function () { return require('vue'); })).defineComponent({
+    setup: function () {
+        return {
+            Header: HeaderComponent_vue_1.default,
+            Main: MainComponent_vue_1.default,
+            Aside: AsideComponent_vue_1.default,
+            Item: ItemComponent_vue_1.default,
+            Article: ArticleComponent_vue_1.default,
+        };
+    },
+});
+exports.default = (await Promise.resolve().then(function () { return require('vue'); })).defineComponent({
+    setup: function () {
+        return {};
+    },
+});
+; /* PartiallyEnd: #4569/main.vue */
---
+++ frontend/src/components/ArticleComponent.vue.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+debugger; /* PartiallyEnd: #3632/scriptSetup.vue */
+var __VLS_ctx = {};
+var __VLS_components;
+var __VLS_directives;
+__VLS_asFunctionalElement(__VLS_intrinsicElements.main, __VLS_intrinsicElements.main)({});
+__VLS_asFunctionalElement(__VLS_intrinsicElements.h1, __VLS_intrinsicElements.h1)({});
+var __VLS_0 = {};
+// @ts-ignore
+var __VLS_1 = __VLS_0;
+var __VLS_dollars;
+var __VLS_self = (await Promise.resolve().then(function () { return require('vue'); })).defineComponent({
+    setup: function () {
+        return {};
+    },
+});
+var __VLS_component = (await Promise.resolve().then(function () { return require('vue'); })).defineComponent({
+    setup: function () {
+        return {};
+    },
+});
+exports.default = {};
+; /* PartiallyEnd: #4569/main.vue */
